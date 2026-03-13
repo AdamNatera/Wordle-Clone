d0a1d6b10f7eadb46ddbbe119533e1c853f821c7
AdamNatera <adamsnatera1@gmail.com>
2026-03-13 12:00:27 -0500

    setup game loop and user input

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
+    # initialize game
+    game_over = False
     wordle = "boner"
-    print("""
+    max_guesses = 6
+    guess_counter = 0
+
+    # game loop
+    while not game_over:
+        print("""
 +---+---+---+---+---+
 |   |   |   |   |   |
 +---+---+---+---+---+
@@ -14,6 +21,24 @@ def main():
 +---+---+---+---+---+
 |   |   |   |   |   |
 +---+---+---+---+---+""")
+        guess_valid = False
     
+        while not guess_valid:
+            guess = input("Enter your guess: ").strip().lower()
+        
+            if len(guess) != 5:
+                print("Error: Guess must contain exactly 5 letters")
+                continue # ask for input again if wrong
+            
+            if not guess.isalpha():
+                print("Error: Guess must only contain letters")
+                continue # ask for input again if wrong
+                
+            break
+
+    
+    
+
+
 if __name__ == "__main__":
     main()
